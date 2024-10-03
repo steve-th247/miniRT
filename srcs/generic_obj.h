ef677188a09eba22d643989e94f8b56022c8a07c
steve-th247 <106506014+steve-th247@users.noreply.github.com>
2024-10-03 18:33:56 +0800

    Add header guard in generic_obj.h

--- srcs/generic_obj.h
+++ srcs/generic_obj.h
@@ -6,11 +6,14 @@
 /*   By: tjien-ji <tjien-ji@42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 20:54:31 by tjien-ji          #+#    #+#             */
-/*   Updated: 2024/09/25 23:10:33 by tjien-ji         ###   ########.fr       */
+/*   Updated: 2024/10/03 18:32:53 by tjien-ji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "vect.h"
+#ifndef GENERIC_OBJ_H
+# define GENERIC_OBJ_H
+
+# include "vect.h"
 
 typedef struct s_generic_obj	t_generic_obj;
 
@@ -26,3 +29,5 @@ void	translate_obj(t_generic_obj *obj, t_vect vector);
 void	rotate_obj(t_generic_obj *obj, t_vect axis, double deg);
 void	resize_obj_1(t_generic_obj *obj, double diff);
 void	resize_obj_2(t_generic_obj *obj, double diff);
+
+#endif
