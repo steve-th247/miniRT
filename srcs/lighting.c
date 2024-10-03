99681c0e327be39e3d79c68ab5121a72badcaaef
steve-th247 <106506014+steve-th247@users.noreply.github.com>
2024-10-03 18:20:57 +0800

    Modify `color_diffuse` to apply 8767d0b for all object types
    Light source now can't shine through an object surface into an
    object when the camera is located inside an object

--- srcs/lighting.c
+++ srcs/lighting.c
@@ -6,7 +6,7 @@
 /*   By: tjien-ji <tjien-ji@42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 11:33:38 by tjien-ji          #+#    #+#             */
-/*   Updated: 2024/09/26 02:39:39 by tjien-ji         ###   ########.fr       */
+/*   Updated: 2024/10/03 18:19:38 by tjien-ji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,8 +32,7 @@ t_color	color_diffuse(t_mlxs *mlxs, t_inter *inter)
 	if (ray2.inter.i == -1 || (vect_is_equal(ray2.inter.point, ray2.pos)))
 	{
 		dot_prod = dot_product(inter->normal, ray2.dir);
-		if (inter->type == PLANE
-			&& dot_product(inter->normal,
+		if (dot_product(inter->normal,
 				sub(inter->point, mlxs->sc->cam.pos)) > 1e-4)
 			dot_prod *= -1;
 		if (dot_prod < 1e-4)
